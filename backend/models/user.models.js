86faab6c6afa73bb0581a9fad6efef8bb2de0e2d
AjaySingh321 <ajaysinghandnikhil@gmail.com>
2026-07-12 11:15:24 +0530

    adding erro and response hadlling and user schema

---
+++ backend/models/user.models.js
@@ -0,0 +1,82 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+const userSchema = new Schema(
+    {
+        fullName: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        refreshTokens: [String],
+        role: {
+            type: String,
+            enum: [
+                "Fleet Manager",
+                "Safety Officer",
+                "Financial Analyst",
+                "Driver",
+            ],
+            required: true,
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+
+    
+    }, { timestamps: true }
+)
+
+userSchema.pre("save", async function () {
+    if (!this.isModified('password')) return;
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+    } catch (err) {
+        console.error(" hashing password is failed", err);
+    }
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: '7d'
+        }
+    )
+}
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            id: this._id,
+            role: this.role,
+            email: this.email
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: '15m'
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
---
+++ backend/utils/apiError.utils.js
@@ -0,0 +1,14 @@
+class apiError extends Error{
+    constructor(statusCode, message="something is wrong", errors = [], stack = ""){
+        super(message)
+        this.statusCode = statusCode
+        this.message = message
+        this.errors = errors
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+export default apiError;
